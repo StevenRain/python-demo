e38aebe7ef953caba791c6590f6aaa26264b652e
steven <renjie373270@163.com>
2018-01-03 22:12:22 +0800

    add mysql injection test

--- src/MySQLDemo.py
+++ src/MySQLDemo.py
@@ -9,42 +9,20 @@ DB = "video"
 
 root = Tk()
 root.title("Python Learning")
-root.geometry('500x200+650+400')
-
-# 用户名
-usernameLabel = Label(root, text="用户名")
-usernameLabel.place(x=10, y=30, width=50, height=20)
-userText = Entry(root)
-userText.place(x=60, y=30, width=300, height=20)
-userText.focus()
-
-# 密码
-usernameLabel = Label(root, text="密码")
-usernameLabel.place(x=10, y=60, width=50, height=20)
-passwordText = Entry(root)
-passwordText.place(x=60, y=60, width=300, height=20)
-passwordText.focus()
-
-# sql 输出
-sqlLabel = Label(root, text="sql")
-sqlLabel.place(x=10, y=90, width=50, height=20)
-sqlText = Entry(root)
-sqlText.place(x=60, y=90, width=300, height=20)
-sqlText.focus()
+root.geometry('520x320+650+400')
 
 
 def exec_sql(sql):
     try:
         connection = connector.connect(host=HOST, user=USER, password=PASSWORD, db=DB)
         cursor = connection.cursor()
-        print("sql : " + sql)
         cursor.execute(sql)
         values = cursor.fetchall()
-        print(values)
         cursor.close()
         connection.close()
+        return values
     except Exception as e:
-        print(e)
+        return e
 
 
 def build_button_pressed():
@@ -58,15 +36,41 @@ def build_button_pressed():
 
 def exec_button_pressed():
     sql = sqlText.get()
-    exec_sql(sql)
+    result = exec_sql(sql)
+    consoleText.delete('1.0', END)
+    consoleText.insert(END, result)
 
+
+# 用户名
+usernameLabel = Label(root, text="用户名")
+usernameLabel.place(x=10, y=30, width=50, height=20)
+userText = Entry(root)
+userText.place(x=60, y=30, width=400, height=20)
+userText.focus()
+
+# 密码
+usernameLabel = Label(root, text="密码")
+usernameLabel.place(x=10, y=60, width=50, height=20)
+passwordText = Entry(root)
+passwordText.place(x=60, y=60, width=400, height=20)
+
+# sql 输出
+sqlLabel = Label(root, text="sql")
+sqlLabel.place(x=10, y=90, width=50, height=20)
+sqlText = Entry(root)
+sqlText.place(x=60, y=90, width=400, height=20)
+
+# print
+consoleText = Text(root, background="black", foreground="green")
+consoleText.place(x=60, y=120, width=400, height=150)
+consoleText.insert(END, "Console Window")
 
 # 生成sql
 submitButton = Button(root, text="生成sql", command=build_button_pressed)
-submitButton.place(x=60, y=120, width=100, height=30)
+submitButton.place(x=60, y=280, width=100, height=30)
 
 # 执行sql
 submitButton = Button(root, text="执行sql", command=exec_button_pressed)
-submitButton.place(x=240, y=120, width=100, height=30)
+submitButton.place(x=240, y=280, width=100, height=30)
 
 root.mainloop()
